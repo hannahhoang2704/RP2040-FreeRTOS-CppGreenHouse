d247f64aad6aa3a4e71148b58268ae9023bd2c3e
Roni <roninu@metropolia.fi>
2024-09-25 10:38:09 +0300

    Sensor handling cleaned up. SDP600 missing.

--- src/taskers/Greenhouse.cpp
+++ src/taskers/Greenhouse.cpp
@@ -3,14 +3,10 @@
 
 using namespace std;
 
-Greenhouse::Greenhouse(shared_ptr<PicoOsUart> uart_sp, shared_ptr<ModbusClient> modbus_client, uint led_pin) :
+Greenhouse::Greenhouse(shared_ptr<PicoOsUart> uart_sp, const shared_ptr<ModbusClient>& modbus_client, uint led_pin) :
         mCLI_UART(std::move(uart_sp)),
-        mHMP60_rh(modbus_client, HMP60_ADDRESS, 256),
-        mHMP60_t(modbus_client, HMP60_ADDRESS, 257),
-        mGMP252_co2_low(modbus_client, GMP252_ADDRESS, 0x0000),
-        mGMP252_co2_high(modbus_client, GMP252_ADDRESS, 0x0001),
-        mGMP252_t_low(modbus_client, GMP252_ADDRESS, 0x0004),
-        mGMP252_t_high(modbus_client, GMP252_ADDRESS, 0x0005),
+        mGMP252(modbus_client),
+        mHMP60(modbus_client),
         // missing pressure sensor
         mMIO12_V(modbus_client, 1, 0),
         mLED(make_shared<LED>(led_pin)) {
@@ -29,7 +25,8 @@ Greenhouse::Greenhouse(shared_ptr<PicoOsUart> uart_sp, shared_ptr<ModbusClient> 
 
 void Greenhouse::automate_greenhouse() {
     mCLI_UART->send("Initiated SENSOR task\n");
-    mMIO12_V.write(100);
+    mMIO12_V.write(0);
+    mMIO12_V.write(0);
     vTaskDelay(pdMS_TO_TICKS(5));
     Logger::log("Initiated GREENHOUSE task\n");
     mFan.write(100);
@@ -38,41 +35,18 @@ void Greenhouse::automate_greenhouse() {
         mLED->toggle();
         mCLI_UART->send("\n");
 
-        update_T();
-        snprintf(msg, 64, " TC = %5.1f C\n", mTempGMP252.f);
-        mCLI_UART->send(msg);
-        snprintf(msg, 64, " TR = %5.1f C\n", mTempHMP60.f);
+        snprintf(msg, 64, " TC = %5.1f C\n", mGMP252.update_temperature());
         mCLI_UART->send(msg);
 
-        snprintf(msg, 64, "  T = %5.1f C\n", update_T());
+        snprintf(msg, 64, "CO2 = %5.1f ppm CO2\n", mGMP252.update_co2());
         mCLI_UART->send(msg);
 
-        snprintf(msg, 64, " RH = %5.1f %%\n", update_RH());
+        snprintf(msg, 64, " TR = %5.1f C\n", mHMP60.update_temperature());
         mCLI_UART->send(msg);
 
-        snprintf(msg, 64, "CO2 = %5.1f ppm CO2\n", update_CO2());
+        snprintf(msg, 64, " RH = %5.1f %%\n", mHMP60.update_relHum());
         mCLI_UART->send(msg);
 
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
-}
-
-float Greenhouse::update_CO2() {
-    mCO2.u32 = mGMP252_co2_low.read();
-    mCO2.u32 += mGMP252_co2_high.read() << 16;
-    return mCO2.f;
-}
-
-float Greenhouse::update_RH() {
-    mRelHum.u16  = mHMP60_rh.read();
-    mRelHum.f /= 10;
-    return mRelHum.f;
-}
-
-float Greenhouse::update_T() {
-    mTempGMP252.u32 = mGMP252_t_low.read();
-    mTempGMP252.u32 += mGMP252_t_high.read() << 16;
-    mTempHMP60.u16 = mHMP60_t.read();
-    //mTempHMP60.f /= 10;
-    return (mTempHMP60.f + mTempGMP252.f) / 2;
 }
--- src/taskers/Greenhouse.h
+++ src/taskers/Greenhouse.h
@@ -9,10 +9,11 @@
 #include "uart/PicoOsUart.h"
 #include "LED.h"
 #include "modbus/ModbusRegister.h"
+#include "Sensor.h"
 
 class Greenhouse {
 public:
-    Greenhouse(std::shared_ptr<PicoOsUart> uart_sp, std::shared_ptr<ModbusClient> modbus_client, uint lep_pin);
+    Greenhouse(std::shared_ptr<PicoOsUart> uart_sp, const std::shared_ptr<ModbusClient>& modbus_client, uint lep_pin);
 
     const int HMP60_ADDRESS{241};
     const int GMP252_ADDRESS{240};
@@ -23,30 +24,13 @@ private:
         object_ptr->automate_greenhouse();
     }
 
-    float update_CO2();
-    float update_RH();
-    float update_T();
-
     TaskHandle_t mTaskHandle;
     std::shared_ptr<PicoOsUart> mCLI_UART; // only for prints -- switch to logger at some point
-    ModbusRegister mHMP60_rh;
-    ModbusRegister mHMP60_t;
-    ModbusRegister mGMP252_co2_low;
-    ModbusRegister mGMP252_co2_high;
-    ModbusRegister mGMP252_t_low;
-    ModbusRegister mGMP252_t_high;
+    Sensor::GMP252 mGMP252;
+    Sensor::HMP60 mHMP60;
     ModbusRegister mMIO12_V;
+
     std::shared_ptr<LED> mLED;
-
-    union u32_to_f_union {
-        uint32_t u32;
-        float f;
-    } mCO2{0}, mTempGMP252{0};
-
-    union u16_to_f_union {
-        uint16_t u16;
-        float f;
-    } mTempHMP60{0}, mRelHum{0};
 };
 
 
