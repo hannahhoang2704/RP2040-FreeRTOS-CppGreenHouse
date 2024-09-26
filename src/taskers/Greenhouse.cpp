a34d5b3b5c5e04a95de4df3f5c3f502431cd5210
Roni <roninu@metropolia.fi>
2024-09-26 08:36:52 +0300

    Rebase merger...

--- src/taskers/Greenhouse.cpp
+++ src/taskers/Greenhouse.cpp
@@ -5,11 +5,14 @@ using namespace std;
 
 Greenhouse::Greenhouse(shared_ptr<PicoOsUart> uart_sp, shared_ptr<ModbusClient> modbus_client, uint led_pin) :
         mCLI_UART(std::move(uart_sp)),
-        mRH_RH(std::move(modbus_client), 241, 256),
-        mT_RH(std::move(modbus_client), 241, 257),
-        // missing CO2 sensor
+        mHMP60_rh(modbus_client, HMP60_ADDRESS, 256),
+        mHMP60_t(modbus_client, HMP60_ADDRESS, 257),
+        mGMP252_co2_low(modbus_client, GMP252_ADDRESS, 0x0000),
+        mGMP252_co2_high(modbus_client, GMP252_ADDRESS, 0x0001),
+        mGMP252_t_low(modbus_client, GMP252_ADDRESS, 0x0004),
+        mGMP252_t_high(modbus_client, GMP252_ADDRESS, 0x0005),
         // missing pressure sensor
-        mFan(std::move(modbus_client), 1, 0),
+        mMIO12_V(modbus_client, 1, 0),
         mLED(make_shared<LED>(led_pin)) {
 
     if (xTaskCreate(task_automate_greenhouse,
@@ -25,16 +28,51 @@ Greenhouse::Greenhouse(shared_ptr<PicoOsUart> uart_sp, shared_ptr<ModbusClient> 
 }
 
 void Greenhouse::automate_greenhouse() {
+    mCLI_UART->send("Initiated SENSOR task\n");
+    mMIO12_V.write(100);
+    vTaskDelay(pdMS_TO_TICKS(5));
     Logger::log("Initiated GREENHOUSE task\n");
     mFan.write(100);
     char msg[64];
     while (true) {
         mLED->toggle();
-        snprintf(msg, 64, "RH=%5.1f%%\n", mRH_RH.read() / 10.0);
+        mCLI_UART->send("\n");
+
+        update_T();
+        snprintf(msg, 64, " TC = %5.1f C\n", mTempGMP252.f);
         mCLI_UART->send(msg);
-        vTaskDelay(5);
-        snprintf(msg, 64, "T =%5.1f%%\n", mT_RH.read() / 10.0);
+        snprintf(msg, 64, " TR = %5.1f C\n", mTempHMP60.f);
         mCLI_UART->send(msg);
-        vTaskDelay(3000);
+
+        snprintf(msg, 64, "  T = %5.1f C\n", update_T());
+        mCLI_UART->send(msg);
+
+        snprintf(msg, 64, " RH = %5.1f %%\n", update_RH());
+        mCLI_UART->send(msg);
+
+        snprintf(msg, 64, "CO2 = %5.1f ppm CO2\n", update_CO2());
+        mCLI_UART->send(msg);
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
+}
+
+float Greenhouse::update_CO2() {
+    mCO2.u32 = mGMP252_co2_low.read();
+    mCO2.u32 += mGMP252_co2_high.read() << 16;
+    return mCO2.f;
+}
+
+float Greenhouse::update_RH() {
+    mRelHum.u16  = mHMP60_rh.read();
+    mRelHum.f /= 10;
+    return mRelHum.f;
+}
+
+float Greenhouse::update_T() {
+    mTempGMP252.u32 = mGMP252_t_low.read();
+    mTempGMP252.u32 += mGMP252_t_high.read() << 16;
+    mTempHMP60.u16 = mHMP60_t.read();
+    //mTempHMP60.f /= 10;
+    return (mTempHMP60.f + mTempGMP252.f) / 2;
 }
--- src/taskers/Greenhouse.h
+++ src/taskers/Greenhouse.h
@@ -13,6 +13,9 @@
 class Greenhouse {
 public:
     Greenhouse(std::shared_ptr<PicoOsUart> uart_sp, std::shared_ptr<ModbusClient> modbus_client, uint lep_pin);
+
+    const int HMP60_ADDRESS{241};
+    const int GMP252_ADDRESS{240};
 private:
     void automate_greenhouse();
     static void task_automate_greenhouse(void * params) {
@@ -20,12 +23,30 @@ private:
         object_ptr->automate_greenhouse();
     }
 
+    float update_CO2();
+    float update_RH();
+    float update_T();
+
     TaskHandle_t mTaskHandle;
     std::shared_ptr<PicoOsUart> mCLI_UART; // only for prints -- switch to logger at some point
-    ModbusRegister mRH_RH;
-    ModbusRegister mT_RH;
-    ModbusRegister mFan;
+    ModbusRegister mHMP60_rh;
+    ModbusRegister mHMP60_t;
+    ModbusRegister mGMP252_co2_low;
+    ModbusRegister mGMP252_co2_high;
+    ModbusRegister mGMP252_t_low;
+    ModbusRegister mGMP252_t_high;
+    ModbusRegister mMIO12_V;
     std::shared_ptr<LED> mLED;
+
+    union u32_to_f_union {
+        uint32_t u32;
+        float f;
+    } mCO2{0}, mTempGMP252{0};
+
+    union u16_to_f_union {
+        uint16_t u16;
+        float f;
+    } mTempHMP60{0}, mRelHum{0};
 };
 
 
