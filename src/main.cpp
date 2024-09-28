e6011bd16694a55aabd985d3c55fb965535587f4
Roni <roninu@metropolia.fi>
2024-09-28 16:44:03 +0300

    Named main parameter values -- C++ style (?)

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,45 @@ uint32_t read_runtime_ctr(void) {
 }
 }
 
+const struct {
+    uint ctrl_nr = 0; // controller number
+    uint tx_pin = 0;
+    uint rx_pin = 1;
+    uint baud = 115200;
+    uint8_t stop_bits = 1;
+} UART0_s;
+
+const struct {
+    uint ctrl_nr = 1; // controller number
+    uint tx_pin = 4;
+    uint rx_pin = 5;
+    uint baud = 9600;
+    uint8_t stop_bits = 2;
+} UART1_s;
+
+const struct {
+    uint ctrl_nr = 1;
+    uint sda_pin = 14;
+    uint scl_pin = 15;
+    uint baud = 400000;
+} I2C1_s;
+
+const struct {
+    uint ctrl_nr = 0;
+    uint sda_pin = 16;
+    uint scl_pin = 17;
+    uint baud = 12345; // ? EEPROM
+} I2C0_s;
+
 using namespace std;
 
 int main() {
     Logger::log("Boot!\n");
 
     /// interfaces
-    auto CLI_UART = make_shared<PicoOsUart>(0,0,1,115200,1);
-    auto modbusUART = make_shared<PicoOsUart>(1,4,5,9600,2);
-    auto OLED_SDP600_I2C = make_shared<PicoI2C>(1,400000);
+    auto CLI_UART = make_shared<PicoOsUart>(UART0_s.ctrl_nr, UART0_s.tx_pin, UART0_s.rx_pin, UART0_s.baud,UART0_s.stop_bits);
+    auto modbusUART = make_shared<PicoOsUart>(UART1_s.ctrl_nr,UART1_s.tx_pin, UART1_s.rx_pin,UART1_s.baud,UART1_s.stop_bits);
+    auto OLED_SDP600_I2C = make_shared<PicoI2C>(I2C1_s.ctrl_nr, I2C1_s.baud);
     auto rtu_client = make_shared<ModbusClient>(modbusUART);
 
     /// taskers
